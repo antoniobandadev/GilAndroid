785d3675800de3227d4440cb9841c4878d068e40
Antonio Banda <antonio.banda.dev@gmail.com>
2025-06-26 23:49:20 -0600

    Clean project v2 20250626

--- app/src/main/java/com/jbg/gil/features/scan/ui/ScannerFragment.kt
+++ app/src/main/java/com/jbg/gil/features/scan/ui/ScannerFragment.kt
@@ -1,7 +1,6 @@
 package com.jbg.gil.features.scan.ui
 
 import android.Manifest
-import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -11,7 +10,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
